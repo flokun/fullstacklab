95889e4bd469bd844676693cde6ed247b2423e31
Florent Val <florent.val@etu.univ-lehavre.fr>
2020-01-02 16:35:03 +0100

    test: add postgresql as service + test getAllBienImmobilier

---
+++ api/tests/BienImmobilierTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use App\Entity\BienImmobilier;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class BienImmobilierTest extends KernelTestCase
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    public function getAllBienImmobilier() {
+        $biensImmobilier = $this->entityManager->getRepository(BienImmobilier::class)->findAll();
+        $this->assertGreaterThan(1, $biensImmobilier);
+    }
+}
