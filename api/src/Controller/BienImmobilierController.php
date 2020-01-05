4b4f32ea34774889da25c51cf64031ae0b90f2e7
Florent Val <florent.val@etu.univ-lehavre.fr>
2020-01-05 22:44:18 +0100

    feat(front): bootstrap 4, couleurs et modification de la mise en page pour le diagramme circulaire et ajout d'une navbar

--- api/src/Controller/BienImmobilierController.php
+++ api/src/Controller/BienImmobilierController.php
@@ -45,55 +45,72 @@ class BienImmobilierController extends AbstractController
 
         $regions = [
             "Auvergnes-Rhônes-Alpes" => [
-                "dept" => [69, 1, 3, 7, 15, 26, 38, 42, 43, 63, 74, 73]
+                "dept" => [69, 1, 3, 7, 15, 26, 38, 42, 43, 63, 74, 73],
+                'color' => 'blue',
             ],
             "Bourgogne-Franche-Compté" => [
-                "dept" => [21, 25, 39, 58, 70, 71, 89, 90]
+                "dept" => [21, 25, 39, 58, 70, 71, 89, 90],
+                'color' => 'yellow',
             ],
             "Bretagne" => [
-                "dept" => [22, 29, 35, 56]
+                "dept" => [22, 29, 35, 56],
+                'color' => 'orange',
             ],
             "Centre-Val de Loire" => [
-                "dept" => [18, 28, 36, 37, 41, 45]
+                "dept" => [18, 28, 36, 37, 41, 45],
+                'color' => 'green',
             ],
             "Grand Est" => [
-                "dept" => [8, 10, 51, 52, 54, 55, 57, 67, 68, 88]
+                "dept" => [8, 10, 51, 52, 54, 55, 57, 67, 68, 88],
+                'color' => 'pink',
             ],
             "Hauts-de-France" => [
-                "dept" => [2, 59, 60, 62, 80]
+                "dept" => [2, 59, 60, 62, 80],
+                'color' => 'gray',
             ],
             "Île-de-France" => [
-                "dept" => [77, 78, 91, 92, 93, 94, 75, 95]
+                "dept" => [77, 78, 91, 92, 93, 94, 75, 95],
+                'color' => 'purple',
             ],
             "Normandie" => [
-                "dept" => [14, 27, 50, 61, 76]
+                "dept" => [14, 27, 50, 61, 76],
+                'color' => '#9966ff',
             ],
             "Nouvelle-Aquitaine" => [
-                "dept" => [16, 17, 19, 23, 24, 33, 40, 47, 64, 79, 86, 87]
+                "dept" => [16, 17, 19, 23, 24, 33, 40, 47, 64, 79, 86, 87],
+                'color' => '#7733ff',
             ],
             "Occitanie" => [
-                "dept" => [9, 11, 12, 30, 31, 32, 34, 46, 48, 65, 66, 81, 82]
+                "dept" => [9, 11, 12, 30, 31, 32, 34, 46, 48, 65, 66, 81, 82],
+                'color' => '#66a33ff',
             ],
             "Pays de la Loire" => [
-                "dept" => [44, 49, 53, 72, 85]
+                "dept" => [44, 49, 53, 72, 85],
+                'color' => 'black',
             ],
             "Provences-Alpes-Côte d'Azur" => [
-                "dept" => [04, 05, 06, 13, 83, 84]
+                "dept" => [04, 05, 06, 13, 83, 84],
+                'color' => 'brown',
             ],
             "Guadeloupe" => [
-                "dept" => [971]
+                "dept" => [971],
+                'color' => '#d966ff',
             ],
             "Martinique" => [
-                "dept" => [972]
+                "dept" => [972],
+                'color' => '#333300',
             ],
             "Guyane" => [
-                "dept" => [973]
+                "dept" => [973],
+                'color' => '#003366',
             ],
             "La Réunion" => [
-                "dept" => [974]
+                "dept" => [974],
+                'color' => '#800000',
             ],
             "Mayotte" => [
-                "dept" => [976]
+                "dept" => [976],
+                'color' => '#00ff00',
             ]
         ];
 
@@ -119,7 +136,9 @@ class BienImmobilierController extends AbstractController
 
         //Calcul du pourcentage
         foreach ($bienImmobiliersSortedByRegion as $key => $venteRegion) {
-            $bienImmobiliersSortedByRegion[$key] = round(($venteRegion / $totalVentes) * 100, 2);
+            $bienImmobiliersSortedByRegion[$key] = [];
+            $bienImmobiliersSortedByRegion[$key]['value'] = round(($venteRegion / $totalVentes) * 100, 2);
+            $bienImmobiliersSortedByRegion[$key]['color'] = $regions[$key]['color'];
         }
 
         return new JsonResponse($bienImmobiliersSortedByRegion, 200);
