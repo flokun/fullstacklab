03de729f00d470ed3c35ad5a683b86634b564201
Florent Val <florent.val@etu.univ-lehavre.fr>
2020-01-07 13:06:18 +0100

    test: add test api

--- api/tests/PageTest.php
+++ api/tests/PageTest.php
@@ -13,4 +13,18 @@ class PageTest extends WebTestCase
         $client->request('GET', '/bien_immobiliers/ventes_regions/2015');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    /** @test */
+    public function checkSucessPriceByMonthYear(){
+        $client = static::createClient();
+        $client->request('GET', '/bien_immobiliers/priceByMonthYear');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    /** @test */
+    public function checkSucessVentesPeriod(){
+        $client = static::createClient();
+        $client->request('GET', '/bien_immobiliers/ventes_period/month/2015-01-01/2015-12-31');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
 }
