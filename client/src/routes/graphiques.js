5cc406efbf60bc73bcadeef99a3b1feb8151b757
Antoine Joublin <antoine.prevost@etu.univ-lehavre.fr>
2020-01-07 09:34:53 +0100

    Ajout de la route du diagramme à baton à la navbar

--- client/src/routes/graphiques.js
+++ client/src/routes/graphiques.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Circulaire from '../components/graphiques/circulaire/index';
+import Barres from '../components/graphiques/barres/index';
 
 export default [
   <Route path="/graphiques/circulaire" component={Circulaire} exact key="circulaire" />,
+  <Route path="/graphiques/barres" component={Barres} exact key="barres" />,
   //TODO: Les deux autres routes pour les autres graphiques (série temporelle et diagramme à barre)
 ];
